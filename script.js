32ac07ad1f53a2f9486784d82fbd9d90ad6967d8
Andersonicman <125220394+Andersonicman@users.noreply.github.com>
2026-01-14 14:11:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var i = 0;
+var a = [];
+function konum(event)
+{
+    
+    var x = event.clientX;
+    var y = event.clientY;
+
+    document.getElementById("x").value = x;
+    document.getElementById("y").value = y;
+
+    a.push(x);
+    a.push(y);
+    
+
+}
+
+function ekle(event)
+{
+    event.stopPropagation()
+    var sresim ;
+    for(var k = 0 ; k < document.getElementsByName("resim").length ; k++)
+    {
+        if(document.getElementsByName("resim")[k].checked)
+        {
+            sresim = document.getElementsByName("resim")[k].value;
+
+        }
+    }
+
+    if(!sresim)
+    {
+        alert("Lütfen bir resim seçiniz!")
+        return;
+    }
+
+    var yazi = "";
+    for(var j = 0 ; j < a.length ; j+=2)
+    {
+        
+        yazi += "<img src = '" + sresim + "' style = 'position: absolute; left:" + a[j] +"px ; top: "+ a[j+1] +"px;' >"
+         
+    }
+    document.body.innerHTML += yazi; 
+}
